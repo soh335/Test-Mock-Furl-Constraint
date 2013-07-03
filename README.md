82e8db8b1551e48951801df31e61a676b41cf386
soh335 <sugarbabe335@gmail.com>
2013-07-03 16:59:09 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::Mock::Furl::Simple - It's new $module
+
+# SYNOPSIS
+
+    use Test::Mock::Furl::Simple;
+
+# DESCRIPTION
+
+Test::Mock::Furl::Simple is ...
+
+# LICENSE
+
+Copyright (C) soh335.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+soh335 <sugarbabe335@gmail.com>
---
+++ lib/Test/Mock/Furl/Simple.pm
@@ -0,0 +1,39 @@
+package Test::Mock::Furl::Simple;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Mock::Furl::Simple - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Mock::Furl::Simple;
+
+=head1 DESCRIPTION
+
+Test::Mock::Furl::Simple is ...
+
+=head1 LICENSE
+
+Copyright (C) soh335.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+soh335 E<lt>sugarbabe335@gmail.comE<gt>
+
+=cut
+
