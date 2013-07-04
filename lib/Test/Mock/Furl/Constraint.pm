753a3c3eb5f707c9538ff12d544505c4dfb7a576
soh335 <sugarbabe335@gmail.com>
2013-07-04 19:09:55 +0900

    refactoring

--- lib/Test/Mock/Furl/Constraint.pm
+++ lib/Test/Mock/Furl/Constraint.pm
@@ -31,21 +31,6 @@ our $EXPECT = {
     },
 };
 
-# ($method, $uri, $opt, $expect)
-sub _parse_args {
-    my $method = shift || "any";
-    my $uri    = shift;
-    my $expect = pop;
-    my $opt    = shift;
-
-    +{
-        expect => $expect,
-        uri    => $uri,
-        opt    => $opt || undef,
-        method => $method,
-    };
-}
-
 sub stub_request {
     my $class  = shift;
     my $cond = _parse_args(@_);
@@ -57,79 +42,6 @@ sub stub_request {
 sub stub_reset {
     my ($class) = @_;
     $EXPECT->{global} = {};
-}
-
-sub __check_query_parameter {
-    my ($expect_query, $url) = @_;
-
-    my @query_form = $url->query_form();
-    my ($ok, $stack) = Test::Deep::cmp_details(\@query_form, $expect_query);
-    unless ( $ok ) {
-        my $msg = Test::Deep::deep_diag($stack);
-        $Tester->croak("query parameter compared is failed\n\n$msg");
-    }
-}
-
-sub __check_headers {
-    my ($expect_headers, $headers) = @_;
-
-    my ($ok, $stack) = Test::Deep::cmp_details($headers, $expect_headers);
-    unless ( $ok ) {
-        my $msg = Test::Deep::deep_diag($stack);
-        $Tester->croak("headers compared is failed\n\n$msg");
-    }
-}
-
-sub __check_content {
-    my ($expect_content, $content) = @_;
-
-    my ($ok, $stack) = Test::Deep::cmp_details($content, $expect_content);
-    unless ( $ok ) {
-        my $msg = Test::Deep::deep_diag($stack);
-        $Tester->croak("content compared is failed\n\n$msg");
-    }
-}
-
-sub __check_method {
-    my ($expect_method, $method) = @_;
-
-    if ( $expect_method eq "any" ) {
-        return 1;
-    }
-    elsif ( uc $expect_method eq uc $method ) {
-        return 1;
-    }
-    else {
-        $Tester->croak("method compare is failed\n\nyour request method is $method. but expect method is $expect_method");
-    }
-}
-
-sub _process {
-    my ($sub, $method, $url, $headers, $content) = @_;
-
-    # method check
-    __check_method($sub->{method}, $method);
-
-    # query parameter check
-    if ( my $expect_query = $sub->{opt}->{query} ) {
-        __check_query_parameter($expect_query, $url);
-    }
-
-    # header check
-    if ( my $expect_headers = $sub->{opt}->{headers} ) {
-        __check_headers($expect_headers, $headers || []);
-    }
-
-    # content check
-    if ( my $expect_content = $sub->{opt}->{content} ) {
-        __check_content($expect_content, $content);
-    }
-
-    my %sub_res = $sub->{expect}->();
-
-    my %merged_res = (%default_response, %sub_res);
-
-    return map { $merged_res{$_} } qw(minor_version status msg headers content);
 }
 
 # install_modifier and install_sub
@@ -190,6 +102,95 @@ sub _process {
             $EXPECT->{Scalar::Util::refaddr(${$self})} = {};
         },
     });
+}
+
+# ($method, $uri, $opt, $expect)
+sub _parse_args {
+    my $method = shift || "any";
+    my $uri    = shift;
+    my $expect = pop;
+    my $opt    = shift;
+
+    +{
+        expect => $expect,
+        uri    => $uri,
+        opt    => $opt || undef,
+        method => $method,
+    };
+}
+
+sub _process {
+    my ($sub, $method, $url, $headers, $content) = @_;
+
+    # method check
+    __check_method($sub->{method}, $method);
+
+    # query parameter check
+    if ( my $expect_query = $sub->{opt}->{query} ) {
+        __check_query_parameter($expect_query, $url);
+    }
+
+    # header check
+    if ( my $expect_headers = $sub->{opt}->{headers} ) {
+        __check_headers($expect_headers, $headers || []);
+    }
+
+    # content check
+    if ( my $expect_content = $sub->{opt}->{content} ) {
+        __check_content($expect_content, $content);
+    }
+
+    my %sub_res = $sub->{expect}->();
+
+    my %merged_res = (%default_response, %sub_res);
+
+    return map { $merged_res{$_} } qw(minor_version status msg headers content);
+}
+
+
+sub __check_query_parameter {
+    my ($expect_query, $url) = @_;
+
+    my @query_form = $url->query_form();
+    my ($ok, $stack) = Test::Deep::cmp_details(\@query_form, $expect_query);
+    unless ( $ok ) {
+        my $msg = Test::Deep::deep_diag($stack);
+        $Tester->croak("query parameter compared is failed\n\n$msg");
+    }
+}
+
+sub __check_headers {
+    my ($expect_headers, $headers) = @_;
+
+    my ($ok, $stack) = Test::Deep::cmp_details($headers, $expect_headers);
+    unless ( $ok ) {
+        my $msg = Test::Deep::deep_diag($stack);
+        $Tester->croak("headers compared is failed\n\n$msg");
+    }
+}
+
+sub __check_content {
+    my ($expect_content, $content) = @_;
+
+    my ($ok, $stack) = Test::Deep::cmp_details($content, $expect_content);
+    unless ( $ok ) {
+        my $msg = Test::Deep::deep_diag($stack);
+        $Tester->croak("content compared is failed\n\n$msg");
+    }
+}
+
+sub __check_method {
+    my ($expect_method, $method) = @_;
+
+    if ( $expect_method eq "any" ) {
+        return 1;
+    }
+    elsif ( uc $expect_method eq uc $method ) {
+        return 1;
+    }
+    else {
+        $Tester->croak("method compare is failed\n\nyour request method is $method. but expect method is $expect_method");
+    }
 }
 
 1;
